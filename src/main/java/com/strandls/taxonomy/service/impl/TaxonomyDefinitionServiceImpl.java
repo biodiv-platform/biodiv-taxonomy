b5b2fba0c58202b394ae90e4f09c0e39beb3d4aa
mekala.ravi <mekala.ravi@strandls.com>
2026-02-05 12:28:19 +0530

    Added print log to see children ids

--- src/main/java/com/strandls/taxonomy/service/impl/TaxonomyDefinitionServiceImpl.java
+++ src/main/java/com/strandls/taxonomy/service/impl/TaxonomyDefinitionServiceImpl.java
@@ -1038,6 +1038,7 @@ public class TaxonomyDefinitionServiceImpl extends AbstractService<TaxonomyDefin
 
 			// Taxonomy Id to be updated for elastic search
 			taxonIds = taxonomyDao.getAllChildren(taxonId);
+			System.out.println(taxonIds);
 			if (taxonIds.size()>0)
 				throw new IllegalArgumentException("This name cannot be converted to a synonym because it has child taxa");
 			
