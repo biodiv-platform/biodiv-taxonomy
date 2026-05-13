d760d3957c3663a738aa625d9276eae5d0ef310b
mekala.ravi <mekala.ravi@strandls.com>
2026-05-13 16:16:54 +0530

    Added additional mapping details for elastic list

--- src/main/java/com/strandls/taxonomy/util/TaxonomyListMinimalData.java
+++ src/main/java/com/strandls/taxonomy/util/TaxonomyListMinimalData.java
@@ -14,20 +14,68 @@ public class TaxonomyListMinimalData {
 
 	@JsonAlias("position")
 	private String position;
+	
+	@JsonAlias("id")
+	private Long id;
+	
+	@JsonAlias("name")
+	private String name;
+	
+	@JsonAlias("rank")
+	private String rank;
+	
+	@JsonAlias("status")
+	private String status;
 
 	public TaxonomyListMinimalData() {
 	}
 
-	public TaxonomyListMinimalData(String position) {
+	public TaxonomyListMinimalData(String position, Long id, String name, String rank, String status) {
 		this.position = position;
+		this.id = id;
+		this.name = name;
+		this.rank = rank;
+		this.status = status;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getPosition() {
 		return position;
 	}
 
-	public void setSpeciesGroup(String position) {
+	public void setPosition(String position) {
 		this.position = position;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getRank() {
+		return rank;
+	}
+
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
 	}
 
 }
